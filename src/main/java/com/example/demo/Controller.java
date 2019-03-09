5a35b00ed9a515737729f2e1314b41ab17a68f42
pavel.soloduha <pavel.soloduha@outlook.com>
2019-03-10 02:53:24 +0300

    add check endpoint and fix compose for nginx

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,11 @@ public class Controller {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.setContentType(new MediaType("text", "plain"));
         return new ResponseEntity<>(token, responseHeaders, HttpStatus.OK);
+    }
+
+    @GetMapping("healthcheck")
+    public String getHealthcheck() {
+        return "I am alive";
     }
 
 }
